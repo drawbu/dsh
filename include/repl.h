e03f43e3d8e6e3f80433e69a0f74bd9a3de23395
Clément <clement2104.boillot@gmail.com>
2023-08-23 13:35:39 +0200

    Get input from repl

--- include/repl.h
+++ include/repl.h
@@ -2,11 +2,18 @@
     #define REPL_H
 
     #include <stdbool.h>
+    #include <stdlib.h>
 
     #include "status.h"
 
 typedef struct {
+    char *input;
+    size_t len;
+} input_t;
+
+typedef struct {
     bool is_running;
+    input_t *input;
 } shell_t;
 
 status_t repl_run(void);
--- src/repl.c
+++ src/repl.c
@@ -1,5 +1,8 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "repl.h"
 #include "status.h"
@@ -11,6 +14,11 @@ shell_t *shell_init(void)
 
     if (shell == NULL)
         return NULL;
+    shell->input = malloc(sizeof(*(shell->input)));
+    if (shell->input == NULL) {
+        free(shell);
+        return NULL;
+    }
     shell->is_running = true;
     return shell;
 }
@@ -18,13 +26,30 @@ shell_t *shell_init(void)
 static
 void shell_free(shell_t *shell)
 {
+    free(shell->input->input);
+    free(shell->input);
     free(shell);
+}
+
+static
+input_t *get_prompt(input_t *input)
+{
+    static size_t offset = 0;
+
+    input->len = getline(&(input->input), &offset, stdin);
+    input->input[input->len - 1] = '\0';
+    return input;
 }
 
 int repl_prompt(shell_t *shell)
 {
-    printf(">\n");
-    shell->is_running = false;
+    input_t *input;
+
+    printf("> ");
+    input = get_prompt(shell->input);
+    printf("input: '%s'\nlen:  %lu\n", input->input, input->len);
+    if (input->len == (size_t)-1 || strcmp(input->input, "exit") == 0)
+        shell->is_running = false;
     return 0;
 }
 
